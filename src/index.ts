22bcb13b670755ae5638e37d5b17a9853c68fabc
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-09-07 05:05:06 +0000

    refactor: use you-died

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,20 @@ import * as ConfigInSqlite3 from '@src/dao/config-in-sqlite3/database'
 import { buildServer } from './server'
 import { PORT, HOST, NODE_ENV, NodeEnv } from '@env'
 import { callNextTickEverySecond } from './schedule'
-
-const tickLoopController = new AbortController()
-
-process.on('exit', () => {
-  tickLoopController.abort()
-
-  ConfigInSqlite3.closeDatabase()
-})
-process.on('SIGHUP', () => process.exit(128 + 1))
-process.on('SIGINT', () => process.exit(128 + 2))
-process.on('SIGTERM', () => process.exit(128 + 15))
+import { youDied } from 'you-died'
 
 go(async () => {
   ConfigInSqlite3.openDatabase()
+  youDied(() => ConfigInSqlite3.closeDatabase())
   await ConfigInSqlite3.prepareDatabase()
 
   const server = buildServer()
   await server.listen(PORT(), HOST())
   if (NODE_ENV() === NodeEnv.Test) process.exit()
 
+  const tickLoopController = new AbortController()
   callNextTickEverySecond(tickLoopController.signal)
+  youDied(() => tickLoopController.abort())
 
   process.send?.('ready')
 })
