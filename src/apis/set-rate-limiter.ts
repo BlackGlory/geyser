78ff6bf328c22a7623ce56e56378e1708cc25c25
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2025-05-10 18:16:40 +0000

    fix: use a transaction for `setRateLimiter`

--- src/apis/set-rate-limiter.ts
+++ src/apis/set-rate-limiter.ts
@@ -2,13 +2,18 @@ import { IRateLimiterConfig } from '@src/contract.js'
 import { setRateLimiterConfiguration } from '@dao/set-rate-limiter-configuration.js'
 import { resetRateLimiter } from '@dao/reset-rate-limiter.js'
 import { eventHub, Event } from '@src/event-hub.js'
+import { getDatabase } from '@src/database.js'
 
 export function setRateLimiter(
   rateLimiterId: string
 , config: IRateLimiterConfig
 ): null {
-  setRateLimiterConfiguration(rateLimiterId, config)
-  resetRateLimiter(rateLimiterId)
+  getDatabase().transaction(() => {
+    setRateLimiterConfiguration(rateLimiterId, config)
+    resetRateLimiter(rateLimiterId)
+  })()
+
   eventHub.emit(rateLimiterId, Event.RateLimiterSet)
+
   return null
 }
